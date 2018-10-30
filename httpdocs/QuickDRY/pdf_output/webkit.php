aa6c82caebf8247d468924e1c2f90e9f318cf227
Ben Kucenski <bkucenski@instituteforsupplymanagement.org>
2018-10-30 10:31:28 -0700

    adding support to render PDF through API rather than directly.  Useful for when your site is hosted on shared hosting and you have another server that you can use for PDF rendering.

--- httpdocs/QuickDRY/pdf_output/webkit.php
+++ httpdocs/QuickDRY/pdf_output/webkit.php
@@ -21,52 +21,58 @@ if (!is_dir(DOC_ROOT_PATH . '/temp/')) {
 }
 
 $html_file = DOC_ROOT_PATH . '/temp/' . $hash . '.html';
-
-$fp = fopen($html_file, 'w');
-fwrite($fp, $Web->HTML);
-fclose($fp);
-
-
 $FileName = $html_file . '.pdf';
 
-$params = [];
-$params[] = '--javascript-delay 5000';
-$params[] = '--enable-javascript';
-$params[] = '--disable-smart-shrinking';
-$params[] = '--page-size ' . ($Web->PDFPageSize ? $Web->PDFPageSize : PDF_PAGE_SIZE_LETTER);
+if(defined('PDF_API')) {
+    $res = Curl::Post(PDF_API, 'html=' . urlencode($Web->HTML));
+    $fp = fopen($FileName, 'w');
+    fwrite($fp, $res->Body);
+    fclose($fp);
+} else {
+
+    $fp = fopen($html_file, 'w');
+    fwrite($fp, $Web->HTML);
+    fclose($fp);
+
+    $params = [];
+    $params[] = '--javascript-delay 5000';
+    $params[] = '--enable-javascript';
+    $params[] = '--disable-smart-shrinking';
+    $params[] = '--page-size ' . ($Web->PDFPageSize ? $Web->PDFPageSize : PDF_PAGE_SIZE_LETTER);
 
 // $params[] = '--debug-javascript';
-$params[] = '-O ' . $Web->PDFPageOrientation;
-if($Web->PDFHeader) {
-    $params[] = '--header-html "' . $Web->PDFHeader . '"';
-}
-if($Web->PDFFooter) {
-    $params[] ='--footer-html "' . $Web->PDFFooter . '"';
-}
+    $params[] = '-O ' . $Web->PDFPageOrientation;
+    if ($Web->PDFHeader) {
+        $params[] = '--header-html "' . $Web->PDFHeader . '"';
+    }
+    if ($Web->PDFFooter) {
+        $params[] = '--footer-html "' . $Web->PDFFooter . '"';
+    }
 
 
-$cmd = DOC_ROOT_PATH . '\\QuickDRY\\bin\\wkhtmltopdf.exe ' . implode(' ', $params) . ' ' . $html_file . ' ' . $FileName;
-Log::Insert($cmd);
+    $cmd = DOC_ROOT_PATH . '\\QuickDRY\\bin\\wkhtmltopdf.exe ' . implode(' ', $params) . ' ' . $html_file . ' ' . $FileName;
+    Log::Insert($cmd);
 
-$output = [];
-exec($cmd, $output);
-$output = implode(PHP_EOL, $output);
-
-
-$e = error_get_last();
-if (!is_null($e) && !stristr($e['message'], 'statically')) {
-    Debug::Halt($e);
-}
+    $output = [];
+    exec($cmd, $output);
+    $output = implode(PHP_EOL, $output);
 
 
-if (!file_exists($FileName)) {
-    Debug::Halt(['file not created', 'file' => $FileName, 'cmd' => $cmd, 'output' => $output]);
-}
+    $e = error_get_last();
+    if (!is_null($e) && !stristr($e['message'], 'statically')) {
+        Debug::Halt($e);
+    }
 
-if ($Web->PDFPostRedirect) {
-    header('location: ' . $Web->PDFPostRedirect);
-    unset($Web->PDFPostRedirect);
-    exit();
+
+    if (!file_exists($FileName)) {
+        Debug::Halt(['file not created', 'file' => $FileName, 'cmd' => $cmd, 'output' => $output]);
+    }
+
+    if ($Web->PDFPostRedirect) {
+        header('location: ' . $Web->PDFPostRedirect);
+        unset($Web->PDFPostRedirect);
+        exit();
+    }
 }
 
 if(isset($_SERVER['HTTP_HOST'])) {
