938e41f6a3cdaf0f1b9dfb10c1002353f63fea0b
Ben Kucenski <bkucenski@instituteforsupplymanagement.org>
2019-01-14 15:32:51 -0700

    fixing modal header

--- httpdocs/QuickDRY/connectors/SQLCodeGen.php
+++ httpdocs/QuickDRY/connectors/SQLCodeGen.php
@@ -1362,8 +1362,8 @@ HTTP::ExitJSON($returnvals);
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
+                <h5 class="modal-title" id="' . $c_name . '_dialog_title"></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h3 class="modal-title" id="' . $c_name . '_dialog_title"></h3>
             </div>
             <div class="modal-body">
 
