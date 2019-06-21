47f5aebd35c729c239aec61af0ced33ab12b6ea1
Ben Kucenski <bkucenski@instituteforsupplymanagement.org>
2019-06-21 11:39:26 -0700

    escaping notice and error messages so that the resulting string doesn't mess with the javascript to display the message to the user

--- httpdocs/QuickDRY/utilities/HTTP.php
+++ httpdocs/QuickDRY/utilities/HTTP.php
@@ -1,23 +1,25 @@
 <?php
+
 class HTTP extends SafeClass
 {
     public static function RemoveParameters($query_str, $params)
     {
         parse_str($query_str, $get);
         foreach ($params as $param) {
-            if(!isset($get[$param])) {
+            if (!isset($get[$param])) {
                 continue;
             }
             unset($get[$param]);
         }
         return http_build_query($get);
     }
+
     /**
      * @return bool
      */
     public static function IsSecure()
     {
-        if(defined('HTTP_HOST_IS_SECURE') && HTTP_HOST_IS_SECURE) { // needed for sites running behind a proxy
+        if (defined('HTTP_HOST_IS_SECURE') && HTTP_HOST_IS_SECURE) { // needed for sites running behind a proxy
             return true;
         }
 
@@ -38,8 +40,8 @@ class HTTP extends SafeClass
     public static function ArrayToHTTPQuery($array, $name)
     {
         $res = [];
-        foreach($array as $v) {
-            $res[] = $name .'[]=' . urlencode($v);
+        foreach ($array as $v) {
+            $res[] = $name . '[]=' . urlencode($v);
         }
         return implode('&', $res);
     }
@@ -59,16 +61,16 @@ class HTTP extends SafeClass
      */
     public static function RedirectError($err, $url = '/')
     {
-        if(!isset($_SERVER['HTTP_HOST'])) {
+        if (!isset($_SERVER['HTTP_HOST'])) {
             Log::Insert($err, true);
             return;
         }
 
-        $_SESSION['error'] = serialize($err); // make it compatible with the Session object
+        $_SESSION['error'] = serialize(str_replace('"', '\\"', str_replace('\\', '\\\\', $err))); // make it compatible with the Session object
 
-        if(isset($_SERVER['HTTP_REFERER']))
+        if ($url == '/' && isset($_SERVER['HTTP_REFERER'])) {
             header('location: ' . $_SERVER['HTTP_REFERER']);
-        else {
+        } else {
             header('location: ' . $url);
         }
         exit();
@@ -80,11 +82,11 @@ class HTTP extends SafeClass
      */
     public static function RedirectNotice($notice, $url = '/')
     {
-        $_SESSION['notice'] = serialize($notice); // make it compatible with the Session object
+        $_SESSION['notice'] = serialize(str_replace('"', '\\"', str_replace('\\', '\\\\', $notice))); // make it compatible with the Session object
 
-        if($url === '/' && isset($_SERVER['HTTP_REFERER']))
+        if ($url === '/' && isset($_SERVER['HTTP_REFERER'])) {
             header('location: ' . $_SERVER['HTTP_REFERER']);
-        else {
+        } else {
             header('location: ' . $url);
         }
         exit();
@@ -103,7 +105,8 @@ class HTTP extends SafeClass
      * @param $url
      * @param $title
      */
-    public static function ExitJavascript($url, $title) {
+    public static function ExitJavascript($url, $title)
+    {
         echo 'Redirecting to <a id="redirect_url" href="' . $url . '">' . $title . '</a><script>
     (function() {
         window.location = document.getElementById("redirect_url");
@@ -119,7 +122,7 @@ class HTTP extends SafeClass
      */
     public static function ExitJSON($json, $HTTP_STATUS = HTTP_STATUS_OK)
     {
-        if($HTTP_STATUS) {
+        if ($HTTP_STATUS) {
             header('HTTP/1.1 ' . $HTTP_STATUS . ': ' . HTTPStatus::GetDescription($HTTP_STATUS));
         }
         header('Content-Type: application/json');
@@ -141,10 +144,10 @@ class HTTP extends SafeClass
      */
     public static function ExitFile($content, $filename, $HTTP_STATUS = HTTP_STATUS_OK, $ContentType = null, $Download = true)
     {
-        if($HTTP_STATUS) {
+        if ($HTTP_STATUS) {
             header('HTTP/1.1 ' . $HTTP_STATUS . ': ' . HTTPStatus::GetDescription($HTTP_STATUS));
         }
-        if($Download) {
+        if ($Download) {
             header('Content-Disposition: attachment; filename="' . $filename . '"');
             header('Access-Control-Allow-Origin: *');
             header('Access-Control-Allow-Credentials: true');
@@ -164,11 +167,13 @@ class HTTP extends SafeClass
         }
         exit($content);
     }
+
     /**
      * @param $header
      */
-    public static function AltHeader($header) {
-        if(!defined('NO_HEADERS'))
+    public static function AltHeader($header)
+    {
+        if (!defined('NO_HEADERS'))
             header($header);
     }
 
@@ -179,18 +184,15 @@ class HTTP extends SafeClass
      */
     public static function PostFromSerialized($serialized)
     {
-        $reqs = explode('&',$serialized);
+        $reqs = explode('&', $serialized);
         $post = [];
-        foreach($reqs as $req)
-        {
-            $nk = explode('=',$req);
+        foreach ($reqs as $req) {
+            $nk = explode('=', $req);
             $nk[0] = urldecode($nk[0]);
-            if(substr($nk[0],-2) === '[]')
-            {
-                $nk[0] = substr($nk[0],0,strlen($nk[0]) - 2);
+            if (substr($nk[0], -2) === '[]') {
+                $nk[0] = substr($nk[0], 0, strlen($nk[0]) - 2);
                 $post[$nk[0]][] = urldecode($nk[1]);
-            }
-            else
+            } else
                 $post[$nk[0]] = isset($nk[1]) ? urldecode($nk[1]) : '';
         }
         return $post;
