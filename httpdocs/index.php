6bddf0a06cbd4d9fd083aee664976baf00ddd4ae
ben <bkucenski@gmail.com>
2021-08-14 06:50:09 -0700

    updating index and composer.json

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -4,11 +4,16 @@ if (isset($_HOST)) {
 }
 
 require_once 'settings.php';
-
-require_once 'QuickDRYInstance/QuickDRYInstance.php';
+require_once 'QuickDRYInstance/Defines.php';
 require_once 'QuickDRY/QuickDRY.php';
-
+require_once 'QuickDRYInstance/QuickDRYInstance.php';
 require_once 'common_modules.php';
+
+
+use QuickDRY\Utilities\Debug;
+use QuickDRY\Utilities\ExceptionHandler;
+use QuickDRY\Web\BrowserOS;
+use QuickDRY\Web\Web;
 
 define('IS_MOBILE',BrowserOS::IsMobile());
 define('GUID', GUID());
@@ -17,14 +22,36 @@ ExceptionHandler::Init();
 
 
 $Web = new Web();
-$Web->Init('signin', 'admin', dirname(__FILE__));
+$Web->Init('signin', 'main', dirname(__FILE__));
 $Web->SetSecureMasterPages([MASTERPAGE_DEFAULT]);
+
+
+const UPLOAD_DIR = DOC_ROOT_PATH . 'uploads/';
+
+
+if (file_exists($Web->SettingsFile)) {
+    require_once $Web->SettingsFile;
+} else {
+    if (file_exists('../' . $Web->SettingsFile)) {
+        require_once '../' . $Web->SettingsFile;
+
+    } else {
+        if (file_exists('../httpdocs/' . $Web->SettingsFile)) {
+            require_once '../httpdocs/' . $Web->SettingsFile;
+
+        } else {
+            Debug::Halt($Web->SettingsFile . ' does not exist');
+        }
+    }
+}
+
 $Web->SetURLs();
 
 if (defined('ROUTE_REQUESTS') && ROUTE_REQUESTS) {
     require_once ROUTE_REQUESTS;
     exit;
 }
+
 
 if ($Web->Server->REQUEST_URI) {
 
